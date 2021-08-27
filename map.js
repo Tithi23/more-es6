7e81d19c35659b9fc53bfe2af1e796e71367d3da
tithi23 <tithiratry@gmail.com>
2021-08-27 05:57:59 -0700

    added more es6 problem solution

---
+++ map.js
@@ -0,0 +1,38 @@
+const numbers = [4, 6, 8, 10];
+const output = [];
+for (const number of numbers) {
+    const result = number * 2;
+    output.push(result);
+}
+console.log(output);
+
+// function declare kore-
+// function doubleOld(number) {
+//     return number * 2;
+// }
+// new way function-
+const numbers = [4, 6, 8, 10];
+const output2 = [];
+const doubleIt = number => number * 2;
+for (const number of numbers) {
+    const result = doubleIt(number);
+    output2.push(result);
+}
+console.log(output2);
+
+
+// 1. loop through each element
+// 2. for each element call the provided function
+// 3. result for each element will be stored in an array
+
+
+
+// const output = numbers.map(doubleIt);
+// const output = numbers.map(number => number * 2);
+const output = numbers.map(x => x * 2);
+// console.log(output);
+
+const squares = numbers.map(x => x * x);
+console.log(squares);
+
+
